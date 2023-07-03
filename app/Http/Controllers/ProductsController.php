828e4e3841e3f1e97dec300ee8a8b04ce8059ad1
alfian.yuska <alfian.yuska@phiraka.com>
2023-07-03 14:52:38 +0700

    first commit

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductsController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all();
+        return view('products', compact('products'));
+    }
+
+    public function cart()
+    {
+        return view('cart');
+    }
+
+    public function addToCart($id)
+    {
+        $product = Product::findOrFail($id);
+        $cart = session()->get('cart', []);
+        if (isset($cart[$id])) {
+            $cart[$id]['quantity']++;
+        } else {
+            $cart[$id] = [
+                "product_name" => $product->product_name,
+                "photo" => $product->photo,
+                "price" => $product->price,
+                "quantity" => 1
+            ];
+        }
+        session()->put('cart', $cart);
+        return redirect()->back()->with('success', 'Product add to cart successfully');
+    }
+}
